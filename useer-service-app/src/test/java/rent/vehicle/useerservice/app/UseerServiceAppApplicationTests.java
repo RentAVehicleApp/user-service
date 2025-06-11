9e83843f0c15c0743edd95fdcc368d2874e3efa2
LIS-One <FukBenn1@gmail.com>
2025-06-11 16:26:57 +0300

    Initial project setup for user service
    - User domain model (Entity/DTO)
    - Maven configuration
    - PostgreSQL via Docker
    - Basic project structure

---
+++ useer-service-app/src/test/java/rent/vehicle/useerservice/app/UseerServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package rent.vehicle.useerservice.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseerServiceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ user-service-model/src/main/java/rent/vehicle/dto/UserDto.java
@@ -0,0 +1,47 @@
+package rent.vehicle.dto;
+
+import jakarta.validation.constraints.*;
+
+import lombok.Getter;
+import lombok.Setter;
+import rent.vehicle.enums.UserLicenseType;
+
+import java.time.Instant;
+import java.time.LocalDate;
+@Getter
+@Setter
+public class UserDto {
+    @NotNull
+    private long userId;
+
+    @NotBlank
+    private String userFirstName;
+
+    @NotBlank
+    private String userLastName;
+
+    @NotNull
+    @Email
+    private String userEmail;
+
+    @NotBlank
+    @Pattern(regexp = "^\\+?[0-9]{9,15}$")
+    private String userPhoneNumber;
+
+    @NotNull
+    @Past
+    @MinAge(16)            // кастомная аннотация, проверяющая, что возраст ≥ 16 лет
+    private LocalDate userBirthDate;
+
+    @NotNull
+    private UserLicenseType userLicense;
+
+    @NotBlank
+    private String drivingLicenseNumber;
+
+    private Instant createdAt;
+
+    private Instant updatedAt;
+
+
+}
