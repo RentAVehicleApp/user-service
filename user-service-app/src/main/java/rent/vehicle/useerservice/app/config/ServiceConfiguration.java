1e0c9713e36007ec8291c69d53dcf1543b7aea8a
LIS-One <FukBenn1@gmail.com>
2025-07-10 00:46:06 +0300

    feature -added cors

--- user-service-app/src/main/java/rent/vehicle/useerservice/app/config/ServiceConfiguration.java
+++ user-service-app/src/main/java/rent/vehicle/useerservice/app/config/ServiceConfiguration.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ServiceConfiguration {
@@ -15,5 +17,19 @@ public class ServiceConfiguration {
                 .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
                 .setMatchingStrategy(MatchingStrategies.STRICT);
         return modelMapper;
+
 }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*") // Разрешить ВСЕ домены
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
     }
